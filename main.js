b9a1db55313f361141b1ef995023eabcb29bb845
PatrickCodeing <117385304+PatrickCodeing@users.noreply.github.com>
2023-05-17 20:16:07 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function take_snapshot(){
 
 console.log('ml5.version:', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/i1lmL6ZqS/.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/i1lmL6ZqS/model.json',modelLoaded);
 
 function modelLoaded(){
     console.log('Model Loaded');
@@ -26,7 +26,36 @@ function modelLoaded(){
 function speak(){
     var synth = window.speechSynthesis;
     speak_data_1 = 'The first prediction is '+prediction_1;
-    speak_data_2 = 'The second prediction is '+prediction_2;
-    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1);
     synth.speak(utterThis);
+}
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error) {
+        console.error(error)
+    }
+    else{
+        console.log(results);
+        document.getElementById("gesture_name").innerHTML = results[0].label;
+        prediction_1 = results[0].label;
+        speak();
+
+        if(results[0].label == "Amazing")
+        {
+            document.getElementById("gesture_img").innerHTML = "&#128076;";
+        }
+        if(results[0].label == "Best")
+        {
+            document.getElementById("gesture_img").innerHTML = "&#128077;";
+        }
+        if(results[0].label == "Victory")
+        {
+            document.getElementById("gesture_img").innerHTML = "&#9996;";
+        }
+    }
 }
